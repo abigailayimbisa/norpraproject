d327a02cb1d03eb7e1edde7967058dce78032d0f
abigailayimbisa <ayimbisaabigailayinsungya@gmail.com>
2024-04-04 13:03:38 +0000

    Contains constants-Abigail

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class colors{
+  Color black = Colors.black;
+  Color white=Colors.white;
+  Color gold= Colors.amber;
+
+}
+
+class Appbar_constants {
+  String text1="";
+  String text2="";
+  String text3="";
+  String text4="";
+}
