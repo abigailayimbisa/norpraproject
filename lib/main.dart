5cef4fec8e59a0f0d934c508a8981cd4c5b21cde
abigailayimbisa <ayimbisaabigailayinsungya@gmail.com>
2024-04-05 15:16:36 +0000

    Contains constants-Abigail-today

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'appbar.dart';
+import 'home.dart';
 
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home:MyAppbar(),
+    home:MyHome(),
+    // home:MyAppbar(),
   ));
 }
